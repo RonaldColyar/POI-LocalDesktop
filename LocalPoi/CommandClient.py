d304bd64db372dfb3e9983d08e06cccc10ec59ac
RonaldColyar <PhantexCreations@gmail.com>
2020-12-24 07:06:36 -0600

    new features and enhancement

--- LocalPoi/CommandClient.py
+++ LocalPoi/CommandClient.py
@@ -22,21 +22,21 @@ class ResultWindow:
             self.formatted_image = tkinter.PhotoImage(file = 'defaultimage.png') #anonymous photo
 
         else:
-            img = str.encode(image)
-            print(type(img))
-            self.formatted_image = tkinter.PhotoImage(master = master,data = img)
+            self.formatted_image = tkinter.PhotoImage(master = master,data = image)
 
         formatted_first = "First Name: " + first
         formatted_last = "Last Name: " + last
         formatted_location = "Location: " + location
-        formatted_reason = "Reason:" + reason
-        formatted_race = "Race:" + race
+        formatted_reason = "Why?:" + reason
+        formatted_race = "Race/Skin Tone:" + race
         #widgets
-        picture = tkinter.Label(master , image = self.formatted_image).pack()
-        first_name = tkinter.Label(master,text = formatted_first).pack()
-        last_name = tkinter.Label(master,text = formatted_first).pack()
-        known_location = tkinter.Label(master, text = formatted_location).pack()
-        reason_why = tkinter.Message(master,text= formatted_reason).pack()
+        master.config(bg = "#222")
+        picture = tkinter.Label(master , image = self.formatted_image,bg = "#222" , fg = "#fff" ).pack()
+        first_name = tkinter.Label(master,text = formatted_first,bg = "#222", fg = "#fff",font=("Courier", 20)).pack()
+        last_name = tkinter.Label(master,text = formatted_last,bg = "#222", fg = "#fff",font=("Courier", 20)).pack()
+        race = tkinter.Label(master,text = formatted_race,bg = "#222", fg = "#fff",font=("Courier", 20)).pack()
+        known_location = tkinter.Label(master, text = formatted_location,bg = "#222", fg = "#fff",font=("Courier", 15)).pack()
+        reason_why = tkinter.Message(master,text= formatted_reason,bg = "#222", fg = "#fff",font=("Courier", 10)).pack()
 
 
     def display_tree(self,data):
@@ -98,13 +98,14 @@ class Client:
             lastname = input("Last Name:")
             location = input("Location:")
             reason = input("Reason:")
-            race = input("Race/Skin Tone(ignore if you have picture):")
+            race = input("Race/Skin Tone:")
             profile_data = {"type" : "PROFILE_CREATION","firstname" : firstname,"lastname" : lastname,
                          "location" : location,"reason" : reason,"race" : race
                         }
             picture_prompt = input("would you like to include a picture?[y/n]:")
             if picture_prompt == "y" or  picture_prompt == "Y":
-                self.profile_creation_with_image(profile_data)
+               profile_data["image"] =self.select_image()
+               self.send_request(profile_data,"profile_creation")
             else:
                 profile_data["image"] = "NOT_PRESENT"
                 self.send_request(profile_data,"profile_creation")
@@ -112,25 +113,42 @@ class Client:
     def display_profile(self,response):
 
         new_dict = json.loads(response)
-        master = tkinter.Toplevel()
+        master = tkinter.Tk()
         result_window = ResultWindow(master,new_dict["firstname"],new_dict["lastname"],
                                      new_dict["location"],new_dict["reason"],new_dict["race"],new_dict["image"])
         master.mainloop()
-    def check_response(self,request,response):
-        if request == "profile_creation" :
-            if response  == "ACCEPTED_CREATION":
-                print("Profile Successfully Created!! ,View it with the command(view profile)")
-            else:
-                print("Error From Server!")
-        elif request == "profile_view":
-         
-            if response == "PROFILE_FOUND":
+
+
+    def check_profile_exist_reponse(self,response):
+        if response == "PROFILE_FOUND":
                 data_size = int(str(self.client.recv(70).decode("ascii")))#size
                 self.client.send("GOT".encode("ascii"))
                 data = self.client.recv(data_size).decode("ascii")
                 self.display_profile(data)
-            else:
-                print("Profile Doesn't exist!")
+        else:
+            print("Profile Doesn't exist!")
+
+    def check_status_response(self,response ,server_accept_message,success_message):
+        if response == server_accept_message:
+                print(success_message)
+        else:
+                print("Error From Server!")
+
+
+    def check_response(self,request,response):
+        if request == "profile_creation" :
+            self.check_status_response(response,"ACCEPTED_CREATION",
+                "Profile Successfully Created!! ,View it with the command(view)")
+        elif request == "profile_view":
+            self.check_profile_exist_reponse(response)
+            
+        elif request == "profile_deletion":
+            self.check_status_response(response,"DELETION_ACCEPTED",colored("Profile Successfully Removed!!" , "red"))
+        elif request == "entry_request" :
+            self.check_status_response(response,"ENTRY_ACCEPTED" ,
+             colored("Entry Successfully Added!! View it with the command(view)" , "green"))
+
+
                 
 
 
@@ -142,12 +160,11 @@ class Client:
         response = self.client.recv(70).decode("ascii")
         self.check_response(message_type,response)
 
-    def profile_creation_with_image(self,user_data):
-        profile_path = filedialog.askopenfilename( initialdir= "/",title='Persons Image')
+    def select_image(self):
+        profile_path = filedialog.askopenfilename( initialdir= "/",title='Persons Image' , filetypes = [("PNG FILES" , "*.png")])
         with open(profile_path, "rb") as file:
-            encoded_image = base64.b64encode(file.read())
-        user_data["image"] = str(encoded_image)
-        self.send_request(user_data,"profile_creation")
+            encoded_image = base64.b64encode(file.read()).decode("ascii")
+        return encoded_image
 
     def view_profile(self):
         first_name = input("First Name:")
@@ -159,49 +176,93 @@ class Client:
         }
         self.send_request(profile_data,"profile_view")
 
-        
     def check_edit_response(self,response):
         if response == "SUCCESSFUL_EDIT":
-            pass
+            print("You Have Successfully Edited The Profile!")
         else:
-            pass
+            print("Issue Editing Profile!")
+
+    
     def send_edit_request(self,edit_data):
         data = json.dumps(edit_data)
         data_size = len(data.encode("ascii"))
-        self.client.send(data_size.encode("ascii"))
+        self.client.send(str(data_size).encode("ascii"))
         self.client.send(data.encode("ascii"))
-        incoming_size = int(self.client.recv(100).decode("ascii"))
-        if incoming_size == 0:
-            pass
-        else:
-            reponse =   self.client.recv(incoming_size).decode("ascii")
-            self.check_edit_response(response)
-
+        response = self.client.recv(100).decode("ascii")
+        self.check_edit_response(response)
 
     def edit_profile(self):
         first = input("First Name:")
         last = input("Last Name:")
         field = input("What Field Would you like to edit/update?:")
-        new_value = input(f"New Field Value for {first + '' + last + field}: ")
-        edit_data = {
-            "type" : "PROFILE_EDIT",
-            "firstname" : first,
-            "lastname"  : last,
-            "field" : field,
-            "value":new_value
-        }
+        if field == "image":
+            image = self.select_image()
+            edit_data = {
+            "type" : "PROFILE_EDIT","firstname" : first,"lastname"  : last,
+            "field" : field,"value":image
+                }
+        else:
+            new_value = input(f"New Field Value for {first + '' + last + field}: ")
+            edit_data = {
+                "type" : "PROFILE_EDIT", "firstname" : first, "lastname"  : last,
+                "field" : field,"value":new_value
+                  }
+        self.send_edit_request(edit_data)
+
+
+    def delete_profile(self):
+        first = input("First Name:")
+        last = input("Last Name:")
+        confirmation = input("Are you sure?[y/n]")
+        if confirmation == "y" or confirmation == "Y":
+            delete_data = {
+              "type" : "REQUEST_DELETION",
+              "firstname" : first,
+              "lastname" : last,
+            }
+            self.send_request(delete_data,"profile_deletion" )
+        else:
+            print(colored("Aborted Deletion!"))
+
+
+    def help(self):
+        print(colored("[1]","green") 
+            + " create-Allows you to create a new profile")
+        print(colored("[2]","green") 
+            + " delete-Allows you to delete a profile")
+        print(colored("[3]","green") 
+            + " view-Allows you to view a profile in a gui")
+        print(colored("[4]","green") 
+            + " edit-Allows you to edit a profile's attribute or add a new attribute!")
         
-
-
+    def add_entry(self):
+        first = input("First Name:")
+        last = input("Last Name:")
+        label = input("Label of entry(unique):")
+        data = input("Data:")
+        entry_data = {
+            "type": "ENTRY_REQUEST",
+            "firstname":first,
+            "lastname":last,
+            "label" : label,
+            "data" : data
+        }
+        self.send_request(entry_data , "entry_request")
 
 
     def command_check(self,command):
-        if  command == "create profile":
+        if  command == "create" or command == "1":
             self.create_profile()
-        elif command == "view profile":
+        elif command == "view" or command == "3":
             self.view_profile()
-        elif command == "edit":
+        elif command == "edit" or command == "4":
             self.edit_profile()
+        elif command == "delete" or command == "2":
+            self.delete_profile()
+        elif command == "entry":
+            self.add_entry()
+        elif command == "--help" or command == "help":
+            self.help()
         else:
             print("unknow command")
 
@@ -213,7 +274,10 @@ class Client:
     def Start(self):
         self.display_welcome_message()
         while self.running == True:
+                print(" ")
+                print(colored("#Persons Of Interest~","green") + "Version 1.0" )
                 command = input(">")
+                print(" ")
                 self.command_check(command)
 
 
--- LocalPoi/Server.py
+++ LocalPoi/Server.py
@@ -8,81 +8,118 @@ this version should only be used to build profiles on
 your local machine , for remote use check the 'CommandClientRemote' client
 """
 class MongoHandler:
-	def __init__(self):
-		self.client = pymongo.MongoClient("mongodb://localhost:27017/")
-		self.db = self.client['persons']
-		self.collection = self.db["profiles"]
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db = self.client['persons']
+        self.collection = self.db["profiles"]
 
-	def create_profile(self,data,client):
-		del data["type"]
-		result = self.collection.insert_one(data)
-		status = result.acknowledged 
-		if status == True :#if the insertion was successful
-			client.send("ACCEPTED_CREATION".encode('ascii'))
-		else:
-			client.send("error".encode("ascii"))
+    def send_crud_status(self,client,message,status):
+        if status == True :#if the crud operation was successful
+            client.send(message.encode('ascii'))
+        else:
+            client.send("error".encode("ascii"))
 
-	def send_profile_data(self,data,client):
-		del data["type"] # we only need the first/last name for query
-		profile = self.collection.find_one(data,{"_id" :0 })
-		if profile == None:
-			client.send("error".encode("ascii"))
-		else:
-			formatted_profile = json.dumps(profile) #convert to string
-			data_size = len(formatted_profile.encode("ascii"))
-			client.send("PROFILE_FOUND".encode("ascii"))
-			client.send(str(data_size).encode("ascii")) #send size of the data for clean data transfer
-			response = client.recv(70).decode("ascii")
-			if response == "GOT":
-				client.send(formatted_profile.encode("ascii"))
-				print(formatted_profile)
-				print(data_size)
+    def create_profile(self,data,client):
+        del data["type"]
+        result = self.collection.insert_one(data)
+        status = result.acknowledged 
+        self.send_crud_status(client,"ACCEPTED_CREATION",status)
+    
+    def send_profile_data(self,data,client):
+        del data["type"] # we only need the first/last name for query
+        profile = self.collection.find_one(data,{"_id" :0 })
+
+        if profile == None:
+            client.send("error".encode("ascii"))
+        else:
+            formatted_profile = json.dumps(profile) #convert to string
+            data_size = len(formatted_profile.encode("ascii"))
+            client.send("PROFILE_FOUND".encode("ascii"))
+            client.send(str(data_size).encode("ascii")) #send size of the data for clean data transfer
+            response = client.recv(70).decode("ascii")
+
+            if response == "GOT":
+                client.send(formatted_profile.encode("ascii"))
+
+    def edit_profile_data(self,data,client):
+        query = {"firstname" : data["firstname"],
+                 "lastname" : data["lastname"]}
+
+        new_value = {"$set" :{data["field"] : data["value"]}}
+        status = self.collection.update_one(query,new_value).acknowledged
+        self.send_crud_status(client,"SUCCESSFUL_EDIT",status)
+
+    def delete_profile(self,data,client):
+        del data["type"]
+        status = self.collection.delete_one(data).acknowledged
+        self.send_crud_status(client,"DELETION_ACCEPTED",status)
+
+    def add_entry(self,data,client):
+        query = {
+            "firstname" : data["firstname"],
+            "lastname" : data["lastname"]
+        }
+
+        path ="entries."+data["label"] #creating a child structure 
+        new_value = {"$set" :{path : data["data"]}} #setting new value(must be unique)
+
+        status = self.collection.update_one(query ,new_value  )
+        self.send_crud_status(client,"ENTRY_ACCEPTED")
+        
+
+
 
 
 class Server :
-	def __init__(self):
-		self.host = '127.0.0.1' #localhost
-		self.port = 50222
-		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.running = True
-		self.mongo_handler = MongoHandler()
+    def __init__(self):
+        self.host = '127.0.0.1' #localhost
+        self.port = 50222
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.running = True
+        self.mongo_handler = MongoHandler()
+        
+    def route_type(self, data,client,thread_running,addr):
+        try:
+            if data["type"] == "PROFILE_CREATION" :
+                self.mongo_handler.create_profile(data,client)
+            elif data["type"] == "PROFILE_REQUEST":
+                self.mongo_handler.send_profile_data(data,client)
+            elif data["type"] == "PROFILE_EDIT":
+                self.mongo_handler.edit_profile_data(data,client)
+            elif data["type"] == "REQUEST_DELETION":
+                self.mongo_handler.delete_profile(data,client)
 
-	def route_type(self, data,client,thread_running,addr):
-		try:
-			if data["type"] == "PROFILE_CREATION" :
-				self.mongo_handler.create_profile(data,client)
-			elif data["type"] == "PROFILE_REQUEST":
-				self.mongo_handler.send_profile_data(data,client)
-				
-		except Exception as e:
-			print("issue")
-			print(e)
+                
+        except Exception as e:
+            print("issue")
+            print(e)
 
-		
-	def client_thread(self,client , addr ):
-		thread_running = True
-		while thread_running == True :
-			data_size = int(str(client.recv(100).decode("ascii")))
-			data = client.recv(data_size).decode("ascii")
-			new_dict = json.loads(data)
+        
+    def client_thread(self,client , addr ):
+        thread_running = True
+        while thread_running == True :
+            data_size = int(str(client.recv(100).decode("ascii")))
+            data = client.recv(data_size).decode("ascii")
+            new_dict = json.loads(data)
 
-			if new_dict["type"] == "DICONNECT_":
-				thread_running = False
-			else:
-				self.route_type(new_dict,client,thread_running,addr)
+            if new_dict["type"] == "DICONNECT_":
+                thread_running = False
+            else:
+                self.route_type(new_dict,client,thread_running,addr)
 
-	def start_server(self):
-		self.server.bind((self.host,self.port))
-		self.server.listen()
-		while self.running == True:
-			client , addr = self.server.accept()
-			#start new thread to avoid blocking new connections!
-			thread = threading.Thread(target =self.client_thread, args = (client,addr,))
-			thread.start()
+    def start_server(self):
+        self.server.bind((self.host,self.port))
+        self.server.listen()
+        while self.running == True:
+            client , addr = self.server.accept()
+            #start new thread to avoid blocking new connections!
+            thread = threading.Thread(target =self.client_thread, args = (client,addr,))
+            thread.start()
 
 
-server = Server()
-server.start_server()
+if __name__ == '__main__':
+    server = Server()
+    server.start_server()
 
-			
+            
 
